60ce1a92afc5489d9261b5bf9f99fc655e60bcda
Malcolm Morgan <mem48@users.noreply.github.com>
2021-01-22 18:04:51 +0000

    Update gtfs2routes.R

--- code/gtfs2routes.R
+++ code/gtfs2routes.R
@@ -1,19 +1,24 @@
 
-files <- list.files("D:/OneDrive - University of Leeds/Data/UK2GTFS/GTFS/gtfs_20200326", full.names = TRUE)
+library(UK2GTFS)
+library(dplyr)
+library(sf)
+library(tmap)
+tmap_mode("plot")
 
-gtfs <- list()
+# files <- list.files("D:/OneDrive - University of Leeds/Data/UK2GTFS/GTFS/gtfs_20200326", full.names = TRUE)
+#
+# gtfs <- list()
+#
+# for(i in 1:length(files)){
+#   message(i)
+#   gtfs[[i]] <- gtfs_read(files[i])
+# }
+#
+# gtfs <- gtfs_merge(gtfs, force = TRUE)
+# gtfs_write(gtfs, "D:/OneDrive - University of Leeds/Data/UK2GTFS/GTFS/gtfs_20200326", "AllGB")
 
-for(i in 1:length(files)){
-  message(i)
-  gtfs[[i]] <- gtfs_read(files[i])
-}
+gtfs <- gtfs_read("D:/OneDrive - University of Leeds/Data/UK2GTFS/GTFS/gtfs_20200326/AllGB.zip")
 
-gtfs <- gtfs_merge(gtfs, force = TRUE)
-gtfs_write(gtfs, "D:/OneDrive - University of Leeds/Data/UK2GTFS/GTFS/gtfs_20200326", "AllGB")
-
-
-# stop_freq <- get_stop_frequency(gtfs, start_hour = 0L, end_hour = 24L,
-#                                 by_route = TRUE)
 
 make_trip_geoms <- function(gtfs){
   stops <- gtfs$stops
@@ -63,7 +68,7 @@ count_stops <- function(gtfs){
   calendar_days <- calendar_days %>%
     dplyr::group_by(service_id) %>%
     dplyr::summarise(runs_extra = sum(exception_type == 1),
-              runs_canceled = sum(exception_type == 2))
+                     runs_canceled = sum(exception_type == 2))
 
   # work out how many times the trip in run
   trips <- dplyr::left_join(trips, calendar, by = "service_id")
@@ -72,13 +77,17 @@ count_stops <- function(gtfs){
   trips$runs_extra[is.na(trips$runs_extra)] <- 0
 
   message("Counting trips on each day")
-  trips$n_monday <- mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Monday")
-  trips$n_tuesday <- mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Tuesday")
-  trips$n_wednesday <- mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Wednesday")
-  trips$n_thursday <- mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Thursday")
-  trips$n_friday <- mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Friday")
-  trips$n_saturday <- mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Saturday")
-  trips$n_sunday <- mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Sunday")
+  future::plan("future::multisession")
+
+  trips$n_monday <- future.apply::future_mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Monday")
+  trips$n_tuesday <- future.apply::future_mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Tuesday")
+  trips$n_wednesday <- future.apply::future_mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Wednesday")
+  trips$n_thursday <- future.apply::future_mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Thursday")
+  trips$n_friday <- future.apply::future_mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Friday")
+  trips$n_saturday <- future.apply::future_mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Saturday")
+  trips$n_sunday <- future.apply::future_mapply(countwd2, startdate = trips$start_date, enddate = trips$end_date, weekday = "Sunday")
+
+  future::plan("future::sequential")
 
   trips$runs_monday <- trips$monday * trips$n_monday
   trips$runs_tuesday <- trips$tuesday * trips$n_tuesday
@@ -100,10 +109,9 @@ count_stops <- function(gtfs){
   stop_times_summary <- stop_times %>%
     dplyr::group_by(stop_id) %>%
     dplyr::summarise(stops_total = sum(runs_total),
-              stops_per_week = sum(runs_per_week))
+                     stops_per_week = sum(runs_per_week))
 
-  stops <- dplyr::left_join(stops, stop_times_summary, by = "stop_id")
-  stops <- sf::st_as_sf(foo, coords = c("stop_lon","stop_lat"), crs = 4326)
+  stops <- dplyr::left_join(gtfs$stops, stop_times_summary, by = "stop_id")
   return(stops)
 }
 
@@ -112,32 +120,49 @@ count_stops <- function(gtfs){
 stops_with_count = count_stops(gtfs)
 summary(stops_with_count$stops_per_week)
 
-tm_shape(stops_with_count) +
-  tm_dots(col = "stops_per_week",
-          size = 0.01,
-          style = "quantile",
-          n = 10,
-          palette = "RdYlBu",
-          border.lwd = NA)
+foo = stops_with_count[!is.na(stops_with_count$stop_lon),]
+foo = foo[!is.infinite(foo$stops_per_week),]
+foo = st_as_sf(foo, coords = c("stop_lon","stop_lat"), crs = 4326)
+summary(foo$stops_per_week)
 
 
 
+# tm_shape(foo) +
+#   tm_dots(col = "stops_per_week",
+#           size = 0.01,
+#           style = "quantile",
+#           n = 10,
+#           palette = "RdYlBu",
+#           border.lwd = NA,
+#           border.alpha = 0)
 
-bounds <- read_sf("data-prepared/LSOA_generalised.gpkg")
-bounds <- st_transform(bounds, 27700)
-foo <- st_transform(foo, 27700)
+
+dir.create("tmp")
+unzip("D:/OneDrive - University of Leeds/Data/OA Bounadries/GB_LSOA_2011_clipped.zip",
+      exdir = "tmp")
+bounds <- read_sf("tmp/infuse_lsoa_lyr_2011_clipped.shp")
+unlink("tmp")
+
+bounds <- st_transform(bounds, 4326)
+
 foo2 <- st_join(foo, bounds)
 foo2 <- st_drop_geometry(foo2)
 foo2 <- foo2 %>%
-  group_by(LSOA11) %>%
-  summarise(stops_total = sum(stops_total),
-            stops_per_week = sum(stops_per_week))
+  dplyr::group_by(geo_code) %>%
+  dplyr::summarise(stops_total = sum(stops_total),
+                   stops_per_week = sum(stops_per_week, na.rm = TRUE))
 
-bounds2 <- left_join(bounds, foo2, by = "LSOA11")
+bounds2 <- dplyr::left_join(bounds, foo2, by = "geo_code")
 bounds2 <- bounds2[!is.na(bounds2$stops_total),]
+bounds2 <- sf::st_transform(bounds2, 27700)
+bounds2 <- sf::st_simplify(bounds2, 100, preserveTopology = TRUE)
+
 tm_shape(bounds2) +
   tm_fill(col = "stops_per_week",
           breaks = quantile(bounds2$stops_per_week, probs = c(seq(0,1,0.1))))
+
+write_sf(bounds2,"lsoa_bus.gpkg")
+write_sf(foo,"bus_stops.gpkg")
 
 # foo = trips_geom[!duplicated(trips_geom$geometry),]
 # qtm(foo, lines.col = "length_km")
